3c38f2e9a80ae99fad8b831204517311d59a09ec
Sơn_SE184220 <sonmtse184220@fpt.edu.vn>
2025-10-17 17:03:27 +0700

    edit profile screen

---
+++ src/app/customer/profile/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import EditProfileScreen from "@/components/(user)/customer/profile/EditProfileScreen";
+
+export default function CustomerProfilePage() {
+    return (
+        <div className="min-h-screen bg-gradient-to-br from-gray-900 via-blue-900 to-black overflow-hidden">
+            <EditProfileScreen />
+        </div>
+    );
+}
