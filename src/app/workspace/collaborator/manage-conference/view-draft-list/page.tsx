be96c0a5c96e86f64f2e558eee2a6e01c1894ea1
theweakant <nam503888@gmail.com>
2025-11-08 20:11:31 +0700

    fix UI create, calendar

---
+++ src/app/workspace/collaborator/manage-conference/view-draft-list/page.tsx
@@ -0,0 +1,5 @@
+import DraftConfenceListPage from "@/components/(user)/workspace/collaborator/ManageConference/DraftConfenceListPage/index";
+
+export default function Page() {
+  return <DraftConfenceListPage />;
+}
