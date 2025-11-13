f3a2868de067da55452ca4fd639752bf81df763a
theweakant <nam503888@gmail.com>
2025-11-13 21:06:05 +0700

    fix run build

--- src/app/workspace/organizer/manage-conference/view-pending-detail/[id]/page.tsx
+++ src/app/workspace/organizer/manage-conference/view-pending-detail/[id]/page.tsx
@@ -1,4 +1,4 @@
-import ConferenceDetailPage from "@/components/(user)/workspace/organizer/ManageConference/ConferenceDetailPage/index";
+import ConferenceDetailPage from "@/components/(user)/workspace/collaborator/ManageConference/ConferenceDetailPage/index";
 
 export default function Page() {
   return <ConferenceDetailPage />;
