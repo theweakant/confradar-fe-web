4f2e64f07527962cbe5da46ce7a68046d3e21a48
theweakant <nam503888@gmail.com>
2025-12-13 21:24:50 +0700

    fix

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,16 +1,5 @@
-// src/firebase/config.ts
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDIMwpdgauDUHd3gxXPXL38904mmK6SRUM",
-//   authDomain: "confradar-762ce.firebaseapp.com",
-//   projectId: "confradar-762ce",
-//   storageBucket: "confradar-762ce.firebasestorage.app",
-//   messagingSenderId: "530552883525",
-//   appId: "1:530552883525:web:cc709e868cd49Bb637d62c",
-//   measurementId: "G-SK24Z55T6L",
-// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyDIMwpdgauDUHd3gxXPXL38904mmK6SRUM",
