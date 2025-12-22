1a6b293e7de16e863b36825d3d922d9ad4e91365
theweakant <nam503888@gmail.com>
2025-12-22 18:11:10 +0700

    fix dashboard reviewer

--- src/types/reviewer.type.ts
+++ src/types/reviewer.type.ts
@@ -24,6 +24,6 @@ export interface ReviewedPapersResponse {
 }
 
 export interface PendingReviewPapersResponse {
-  totalPaperPending: number;
+  totalPendingReview: number;
   paperDetails: PaperDetail[];
 }
